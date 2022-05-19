8620a1ec7dea56f7612c23a9955db7f1d0106507
Matt Evans <mattevo10@gmail.com>
2022-05-19 12:49:13 +0200

    added swagger docs

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,36 @@
-import express from "express";
+import express from "express"
+import swaggerJsdoc from "swagger-jsdoc"
+import swaggerUi from "swagger-ui-express"
 
-const app = express();
+const app = express()
+const customCss = {
+  customCss: ".swagger-ui .topbar { display: none }",
+}
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Climbing Gyms",
+      version: "1.0.0",
+      description: "Api for climbing gyms",
+      license: {
+        name: "Licensed Under MIT",
+        url: "https://spdx.org/licenses/MIT.html",
+      },
+    },
+  },
+  apis: ["./src/*.*"], // files containing annotations as above
+  servers: [
+    {
+      url: "http://localhost:3000",
+      description: "Development server",
+    },
+  ],
+}
+
+const swaggerSpec = swaggerJsdoc(options)
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec, customCss))
 
 const arrayGyms = [
   {
@@ -33,21 +63,95 @@ const arrayGyms = [
       country: "United Kingdom",
     },
   },
-];
+]
+
+/**
+ * @openapi
+ *
+ * /gyms:
+ *    get:
+ *      summary: Retrieve all possible Gyms.
+ *      description: Retrieve all possible Gyms.
+ *      responses:
+ *        200:
+ *          description: Returns all the gyms
+ *          content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: integer
+ *                     description: The Gyms ID.
+ *                     example: 1
+ *                   name:
+ *                     type: string
+ *                     description: The gyms name.
+ *                     example: The Castle
+ *                   location:
+ *                     type: object
+ *                     properties:
+ *                       city:
+ *                         type: string
+ *                         description: The Gyms city
+ *                         example: London
+ *
+ */
 
 app.get("/gyms", (req, res) => {
-  return res.send(Object.values(arrayGyms));
-});
+  return res.send(Object.values(arrayGyms))
+})
+
+/**
+ * @openapi
+ *
+ * /gyms/{id}:
+ *   get:
+ *     summary: Retrieve a single Gym.
+ *     description: Retrieve a single Gym.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: Numeric ID of the gym to retrieve.
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: A single gym.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: integer
+ *                   description: The Gyms ID.
+ *                   example: 1
+ *                 name:
+ *                   type: string
+ *                   description: The gyms name.
+ *                   example: The Castle
+ *                 location:
+ *                   type: object
+ *                   properties:
+ *                     city:
+ *                       type: string
+ *                       description: The Gyms city
+ *                       example: London
+ */
 
 app.get("/gyms/:gymId", (req, res) => {
-  return res.send(arrayGyms.find((x) => x.id === +req.params.gymId));
-});
+  return res.send(arrayGyms.find((x) => x.id === +req.params.gymId))
+})
 
-const port = 3000;
+const port = 3000
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
+  console.log(`Listening on port ${port}`)
+})
 
 /* // middleware
 const hello = (req: express.Request, res: express.Response, next: express.NextFunction) => {
